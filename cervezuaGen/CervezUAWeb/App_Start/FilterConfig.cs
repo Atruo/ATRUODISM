0342d625756d2442e1eee6e36290d3cfa5265e88
unknown <pab23@gcloud.ua.es>
2018-11-20 14:27:11 +0100

    Web añadida2

---
+++ cervezuaGen/CervezUAWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CervezUAWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ cervezuaGen/CervezUAWeb/Controllers/UsuarioController.cs
@@ -0,0 +1,93 @@
+﻿using CervezUAGenNHibernate.CEN.CervezUA;
+using CervezUAGenNHibernate.EN.CervezUA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CervezUAWeb.Controllers
+{
+    public class UsuarioController : BasicController
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            UsuarioCEN usuario = new UsuarioCEN();
+            IEnumerable<UsuarioEN> listaUsuarios = usuario.ReadAll(0, -1).ToList();
+            return View(listaUsuarios);
+        }
+
+        // GET: Usuario/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Usuario/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuario/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuario/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Usuario/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuario/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Usuario/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
